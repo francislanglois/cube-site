132af9aa813fbe0c4c9038fbf54e2ce936486343
Francis Langlois <francis.langlois@gmail.com>
2015-08-24 22:19:39 -0400

    added tablet view for portfolio section

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-$('button').on('click', function(){
+$('nav button').on('click', function(){
   //finds what the button's class is and puts it in a variable currentSide
   var currentSide = $(this).attr('class');
   //remove any class from the cube and adds the class that was in the variable currentSide
@@ -17,4 +17,61 @@ $('button').on('click', function(){
   else {
     $('nav button').css('color', 'rgba(200, 160, 220, 1)')
   }
+});
+
+var currentSelection = '';
+
+function checkWidth(init){
+  if($(window).width()<940){
+    $('.peice-1-button').addClass('selected');
+    $('.peice-2-button').removeClass('selected');
+    $('.peice-3-button').removeClass('selected');
+    $('.peice-1').addClass('displayed-peice');
+    $('.peice-1').removeClass('hidden');
+    $('.peice-2').addClass('hidden');
+    $('.peice-2').removeClass('displayed-peice');
+    $('.peice-3').addClass('hidden');
+    $('.peice-3').removeClass('displayed-peice');
+  }
+  else{
+    if(!init){
+      $('.peice-1').removeClass('displayed-peice');
+      $('.peice-2').removeClass('displayed-peice');
+      $('.peice-3').removeClass('displayed-peice');
+      $('.peice-1').removeClass('hidden');
+      $('.peice-2').removeClass('hidden');
+      $('.peice-3').removeClass('hidden');
+    }
+  }
+}
+$('.peice-buttons button').on('click', function(){
+  $('.peice-1-button').removeClass('selected');
+  $(this).addClass('selected');
+  
+  $(currentSelection).removeClass('selected');
+  currentSelection = $(this);
+  
+  if($(this).hasClass('peice-1-button')){
+    $('.peice-1').removeClass('hidden').addClass('displayed-peice');
+    $('.peice-2').removeClass('displayed-peice').addClass('hidden');
+    $('.peice-3').removeClass('displayed-peice').addClass('hidden');
+  }
+  else if ($(this).hasClass('peice-2-button')){
+    $('.peice-2').removeClass('hidden').addClass('displayed-peice');
+    $('.peice-1').removeClass('displayed-peice').addClass('hidden');
+    $('.peice-3').removeClass('displayed-peice').addClass('hidden');
+  }
+  else if($(this).hasClass('peice-3-button')){
+    $('.peice-3').removeClass('hidden').addClass('displayed-peice');
+    $('.peice-1').removeClass('displayed-peice').addClass('hidden');
+    $('.peice-2').removeClass('displayed-peice').addClass('hidden');
+  }
+});
+
+$(document).ready(function(){
+  checkWidth(true);
+  
+  $(window).resize(function(){
+    checkWidth(false);
+  });
 });
