84efb06a9b4d45d6c5d46781e2d92056ad7180fd
Francis Langlois <francis.langlois@gmail.com>
2015-09-03 16:27:13 -0400

    Added functionality to the left and rigth arrow & refactored functions out of code to keep it DRY

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,36 @@
-var props = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
-prop,
-el = document.createElement('div');
+var yAngle = 0;
 
-for(var i = 0, l = props.length; i < l; i++) {
-  if(typeof el.style[props[i]] !== "undefined") {
-    prop = props[i];
-    break;
+//sets the color scheme of the nav bar based on the yAngle
+function setColorScheme(yAngle){
+  if(yAngle === -90 || yAngle === 270){
+    $('nav button').css('color', 'rgba(75, 170, 200, 1)');
+  }
+  else if (yAngle === -270 || yAngle === 90) {
+    $('nav button').css('color', 'rgba(225, 105, 10, 1)');
+  }
+  else if (yAngle === -180 || yAngle === 180){
+    $('nav button').css('color', 'rgba(215, 20, 75, 1)');
+  }
+  else {
+    $('nav button').css('color', 'rgba(60, 185, 15, 1)');
   }
 }
 
-var yAngle = 0;
+function rotateSides(yAngle){
+  
+  var props = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
+  prop,
+  el = document.createElement('div');
+  
+  for(var i = 0, l = props.length; i < l; i++) {
+    if(typeof el.style[props[i]] !== "undefined") {
+      prop = props[i];
+      break;
+    }
+  }
+  document.getElementById('cube').style[prop] = 'translateZ(-50vw) rotateY('+yAngle+'deg)';
+}
+
 
 $('nav button').on('click', function(){
   
@@ -18,38 +39,34 @@ $('nav button').on('click', function(){
   //remove any class from the cube and adds the class that was in the variable currentSide
   $('#cube').removeClass().addClass(currentSide);
   
+  //if the nav bar has the class of expand (for mobile) then it removes that class to hide the menu after something has been clicked on
   if($('.responsive-menu').hasClass('expand')){
     $('.responsive-menu').removeClass('expand');
   }
   
-  //Changes the color of the nav bar to match the theme when the cube is rotated
+  //sets the yAngle based on which nav bar button has been pushed
   if($(this).attr('class') === 'show-right'){
-    $('nav button').css('color', 'rgba(75, 170, 200, 1)');
-    document.getElementById('cube').style[prop] = 'translateZ(-50vw) rotateY(-90deg)';
     yAngle = -90;
   }
   else if ($(this).attr('class') === 'show-front') {
-    $('nav button').css('color', 'rgba(60, 185, 15, 1)');
-    document.getElementById('cube').style[prop] = 'translateZ(-50vw) rotateY(0deg)';
     yAngle = 0;
   }
   else if ($(this).attr('class') === 'show-left'){
-    $('nav button').css('color', 'rgba(225, 105, 10, 1)');
-    document.getElementById('cube').style[prop] = 'translateZ(-50vw) rotateY(90deg)';
     yAngle = 90;
   }
   else {
-    $('nav button').css('color', 'rgba(215, 20, 75, 1)');
-    document.getElementById('cube').style[prop] = 'translateZ(-50vw) rotateY(180deg)';
     yAngle = 180;
   }
+  
+  rotateSides(yAngle);
+  setColorScheme(yAngle);
 });
 
 $('body').keydown(function(e){
   $('#cube').removeClass('show-right').removeClass('show-front').removeClass('show-left');
   
   switch(e.keyCode){
-    case 37:
+    case 37: //left key press
     yAngle += 90;
     if(yAngle === -360 || yAngle === 360){
       yAngle = 0;
@@ -57,26 +74,34 @@ $('body').keydown(function(e){
     break;
     
     case 39:
-    yAngle -= 90;
+    yAngle -= 90; //right key press
     if(yAngle === -360 || yAngle === 360){
       yAngle = 0;
     };
     break;
   };
-  document.getElementById('cube').style[prop] = 'translateZ(-50vw) rotateY('+yAngle+'deg)';
   
-  if(yAngle === -90 || yAngle === 270){
-    $('nav button').css('color', 'rgba(75, 170, 200, 1)');
-  }
-  else if (yAngle === 180 || yAngle === -180) {
-    $('nav button').css('color', 'rgba(215, 20, 75, 1)');
-  }
-  else if (yAngle === 90 || yAngle === -270){
-    $('nav button').css('color', 'rgba(225, 105, 10, 1)');
+  rotateSides(yAngle);
+  setColorScheme(yAngle);
+});
+
+//rotates the page if one of the chevron arrows is clicked
+$('.arrow-container button').on('click', function(){
+  
+  if ($(this).hasClass('left-arrow')){
+    yAngle += 90;
+    if(yAngle === -360 || yAngle === 360){
+      yAngle = 0;
+    }
   }
   else{
-    $('nav button').css('color', 'rgba(60, 185, 15, 1)');
+    yAngle -= 90;
+    if(yAngle === -360 || yAngle ===360){
+      yAngle = 0;
+    }
   }
+  rotateSides(yAngle);
+  setColorScheme(yAngle);
 });
 
 
@@ -91,21 +116,15 @@ function checkWidth(init){
     $('.peice-1-button').addClass('selected');
     $('.peice-2-button').removeClass('selected');
     $('.peice-3-button').removeClass('selected');
-    $('.peice-1').addClass('displayed-peice');
-    $('.peice-1').removeClass('hidden');
-    $('.peice-2').addClass('hidden');
-    $('.peice-2').removeClass('displayed-peice');
-    $('.peice-3').addClass('hidden');
-    $('.peice-3').removeClass('displayed-peice');
+    $('.peice-1').addClass('displayed-peice').removeClass('hidden');
+    $('.peice-2').addClass('hidden').removeClass('displayed-peice');
+    $('.peice-3').addClass('hidden').removeClass('displayed-peice');
   }
   else{
     if(!init){
-      $('.peice-1').removeClass('displayed-peice');
-      $('.peice-2').removeClass('displayed-peice');
-      $('.peice-3').removeClass('displayed-peice');
-      $('.peice-1').removeClass('hidden');
-      $('.peice-2').removeClass('hidden');
-      $('.peice-3').removeClass('hidden');
+      $('.peice-1').removeClass('displayed-peice').removeClass('hidden');
+      $('.peice-2').removeClass('displayed-peice').removeClass('hidden');
+      $('.peice-3').removeClass('displayed-peice').removeClass('hidden');
     }
   }
 }
@@ -131,6 +150,10 @@ $('.peice-buttons button').on('click', function(){
     $('.peice-1').removeClass('displayed-peice').addClass('hidden');
     $('.peice-2').removeClass('displayed-peice').addClass('hidden');
   }
+});
+
+$('.left-arrow').on('click', function(){
+  
 });
 
 $(document).ready(function(){
